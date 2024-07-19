6ce2c034a7eb505d81376b676b64c2ab992bc53c
henrygagnier <prosock@protonmail.com>
2024-07-18 20:55:52 -0400

    feat: add express

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
